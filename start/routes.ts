3ed76532259a97ae1fde48ad9448af06a151a6ec
Lucas Humbert <lucas.humbert.28@gmail.com>
2025-03-25 15:28:08 +0100

    auth

--- start/routes.ts
+++ start/routes.ts
@@ -8,9 +8,36 @@
 */
 
 import router from '@adonisjs/core/services/router'
+import { middleware } from '#start/kernel'
 
 router.get('/', async () => {
   return {
-    hello: 'world',
+    docto: 'pas libre',
   }
 })
+
+/**
+ *
+ * AUTHENTIFICATION ROUTES
+ *
+ */
+const AuthController = () => import('#controllers/auth_controller')
+
+router
+  .group(() => {
+    router.post('login', [AuthController, 'login'])
+    router.post('register', [AuthController, 'register'])
+    router.post('logout', [AuthController, 'logout']).use(middleware.auth())
+  })
+  .prefix('user')
+
+router
+  .get('me', async ({ auth, response }) => {
+    try {
+      const user = auth.getUserOrFail()
+      return response.ok(user)
+    } catch (error) {
+      return response.unauthorized({ error: 'User not found' })
+    }
+  })
+  .use(middleware.auth())
